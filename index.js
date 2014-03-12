d9540d276c9e8c7bdb0f70828b109a413ee1c92d
Manuel Astudillo <manuel@optimalbits.com>
2014-03-12 12:27:06 +0100

    initial implementation

---
+++ index.js
@@ -0,0 +1,52 @@
+"use strict";
+
+
+var
+  _ = require('lodash'),
+  env = process.env,
+  url = require('url'),
+  validUrl = require('valid-url');
+
+var nodeMode = env.NODE_ENV;
+var contexts = [];
+
+function sconf(conf, development, test, production){
+  if(_.isString(conf)){
+    var prefix = contexts.join('_');
+    var val = env[prefix ? +'_'+conf : conf];
+    var extra;
+    switch(nodeMode){
+      case 'development':
+        extra = development;
+        break;
+      case 'test':
+        extra = test;
+        break;
+      case 'production':
+        extra = production
+        break;
+    }
+    val = val || extra || development || test || production;
+    
+    if(_.isString(val) && validUrl.isUri(val)){
+      return url.parse(val);
+    }else{
+      return val;
+    }
+  } else{
+    if(contexts.length){
+      contexts.pop();
+    }
+    return conf;
+  }
+}
+
+sconf.prefix = function(prefix){
+  // Push a prefix in the contexts stack.
+  contexts.push(prefix);
+  return sconf;
+}
+
+sconf.extend = _.bind(_.extend, _);
+
+module.exports = sconf;
